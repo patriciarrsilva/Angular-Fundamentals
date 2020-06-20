28786dd93603427dbd3237a82b097fc0e0f25ab8
Patrícia Silva <prrs89@gmail.com>
2020-06-20 13:50:25 +0100

    Communicating with Parent Components Using @Output

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,4 +1,9 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter
+} from '@angular/core';
 
 @Component({
   selector: 'event-thumbnail',
@@ -14,13 +19,24 @@ import { Component, Input } from '@angular/core';
         </span>
         <span>&nbsp;</span>
         <span>
-          {{ event.location.city }}, 
+          {{ event.location.city }},
           {{ event.location.country }}
         </span>
       </div>
+      <button
+        class="btn btn-primary"
+        (click)="handleClickMe()"
+      >
+        Click me!
+      </button>
     </div>
   `
 })
 export class EventThumbnailComponent {
   @Input() event: any;
+  @Output() eventClick = new EventEmitter();
+
+  handleClickMe() {
+    this.eventClick.emit(this.event.name);
+  }
 }
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
     <div>
       <h1>Upcoming Angular Events</h1>
       <hr />
-      <event-thumbnail [event]="event1">
+      <event-thumbnail
+        (eventClick)="handleEventClicked($event)"
+        [event]="event1"
+      >
       </event-thumbnail>
     </div>
   `
@@ -25,4 +28,8 @@ export class EventsListComponent {
       country: 'England'
     }
   };
+
+  handleEventClicked(data) {
+    console.log('received:', data);
+  }
 }
