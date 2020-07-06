f5a6640e882a9af18c6ffd2be1dd8f9eec89e89b
Patrícia Silva <prrs89@gmail.com>
2020-07-06 22:42:35 +0100

    Hiding Content with the [Hidden] Binding

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -8,7 +8,7 @@ import { Component, Input } from '@angular/core';
       <div>Date: {{ event?.date }}</div>
       <div>Time: {{ event?.time }}</div>
       <div>Price: \${{ event?.price }}</div>
-      <div *ngIf="event?.location">
+      <div [hidden]="!event?.location">
         <span>
           Location: {{ event?.location?.address }}
         </span>
@@ -17,7 +17,7 @@ import { Component, Input } from '@angular/core';
           {{ event?.location?.country }}
         </span>
       </div>
-      <div *ngIf="event?.onlineUrl">
+      <div [hidden]="!event?.onlineUrl">
         Online URL: {{ event?.onlineUrl }}
       </div>
     </div>
