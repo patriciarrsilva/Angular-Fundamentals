05561c5f9d0988d9521b552a8a6bce111b0051c5
Patrícia Silva <prrs89@gmail.com>
2020-07-14 12:34:10 +0100

    Adding Your First Route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,14 @@ import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
