fbdd20b4d5c075cf990d3bab3ff9b5edd34ff289
Patrícia Silva <prrs89@gmail.com>
2020-08-07 16:48:49 +0100

    Diving Deeper into Template-based forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,8 @@
 import { NgModule } from '@angular/core';
+import {
+  FormsModule,
+  ReactiveFormsModule
+} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
@@ -16,6 +20,8 @@ import { appRoutes } from './routes';
 @NgModule({
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   declarations: [
--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -348,4 +348,10 @@ export class EventService {
   getEvent(id: number): IEvent {
     return EVENTS.find(event => event.id === id);
   }
+
+  saveEvent(event) {
+    event.id = 999;
+    event.session = [];
+    EVENTS.push(event);
+  }
 }
