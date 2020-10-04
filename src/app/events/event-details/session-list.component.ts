09fed22ebae0213b0b70b84638b1f8c9bf91cdef
Patrícia Silva <prrs89@gmail.com>
2020-10-04 19:49:00 +0100

    Adding Voting Functionality

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -3,7 +3,10 @@ import {
   Input,
   OnChanges
 } from '@angular/core';
+
+import { AuthService } from 'src/app/user/auth.service';
 import { ISession } from '../shared/index';
+import { VoterService } from './voter.service';
 
 @Component({
   selector: 'session-list',
@@ -15,6 +18,11 @@ export class SessionListComponent implements OnChanges {
   @Input() sortBy: string;
   visibleSessions: ISession[];
 
+  constructor(
+    private auth: AuthService,
+    private voterService: VoterService
+  ) {}
+
   ngOnChanges() {
     if (this.sessions) {
       this.filterSessions(this.filterBy);
@@ -22,6 +30,31 @@ export class SessionListComponent implements OnChanges {
         ? this.visibleSessions.sort(sortByNameAsc)
         : this.visibleSessions.sort(sortByVotesDesc);
     }
+  }
+
+  toggleVote(session: ISession) {
+    if (this.userHasVoted(session)) {
+      this.voterService.deleteVoter(
+        session,
+        this.auth.currentUser.userName
+      );
+    } else {
+      this.voterService.addVoter(
+        session,
+        this.auth.currentUser.userName
+      );
+    }
+
+    if (this.sortBy === 'votes') {
+      this.visibleSessions.sort(sortByVotesDesc);
+    }
+  }
+
+  userHasVoted(session: ISession) {
+    return this.voterService.userHasVoted(
+      session,
+      this.auth.currentUser.userName
+    );
   }
 
   filterSessions(filter) {
