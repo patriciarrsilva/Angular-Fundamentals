ef9fb4c40fd9a92114d6552967f083d4049302b7
Patrícia Silva <prrs89@gmail.com>
2020-11-09 22:21:31 +0000

    Persisting Authentication Status Across Page Refreshes

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -48,6 +48,19 @@ export class AuthService {
     return !!this.currentUser;
   }
 
+  checkAuthenticationStatus() {
+    this.http
+      .get('/api/currentIdentity')
+      .pipe(
+        tap(data => {
+          if (data !== null) {
+            this.currentUser = <IUser>data;
+          }
+        })
+      )
+      .subscribe();
+  }
+
   updateCurrentUser(firstName: string, lastName: string) {
     this.currentUser.firstName = firstName;
     this.currentUser.lastName = lastName;
