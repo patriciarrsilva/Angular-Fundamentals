4ebc48a86a8c4f2962cb91641d64d658b2b79c28
Patrícia Silva <prrs89@gmail.com>
2020-12-13 17:26:43 +0000

    Unit Testing Services

---
+++ src/app/events/event-details/voter.service.spec.ts
@@ -0,0 +1,34 @@
+import { of } from 'rxjs';
+import { ISession } from '../shared/event.model';
+import { VoterService } from './voter.service';
+
+describe('VoterService', () => {
+  let voterService: VoterService;
+  let mockHttp;
+
+  beforeEach(() => {
+    mockHttp = jasmine.createSpyObj('mockHttp', [
+      'delete',
+      'post'
+    ]);
+
+    voterService = new VoterService(mockHttp);
+  });
+
+  describe('deleteVoter', () => {
+    it('should remove the voter from the list of voters', () => {
+      const session = { id: 6, voters: ['joe', 'john'] };
+
+      mockHttp.delete.and.returnValue(of(false));
+
+      voterService.deleteVoter(
+        3,
+        <ISession>session,
+        'joe'
+      );
+
+      expect(session.voters.length).toBe(1);
+      expect(session.voters[0]).toBe('john');
+    });
+  });
+});
