327ff5bc3a2d1e1085e4874bf22aa50dbe7db2bd
Patrícia Silva <prrs89@gmail.com>
2020-12-20 18:06:24 +0000

    Testing Components with Isolated Tests

---
+++ src/app/events/event-details/session-list.component.spec.ts
@@ -0,0 +1,52 @@
+import { ISession } from '../shared/event.model';
+import { SessionListComponent } from './session-list.component';
+
+describe('SessionListComponent', () => {
+  let component: SessionListComponent;
+
+  beforeEach(() => {
+    const mockAuthService = undefined;
+    const mockVoterService = undefined;
+
+    component = new SessionListComponent(
+      mockAuthService,
+      mockVoterService
+    );
+  });
+
+  describe('ngOnChanges', () => {
+    it('should filter the sessions correctly', () => {
+      component.sessions = <ISession[]>[
+        { name: 'session 1', level: 'intermediate' },
+        { name: 'session 2', level: 'intermediate' },
+        { name: 'session 3', level: 'beginner' }
+      ];
+
+      component.filterBy = 'intermediate';
+      component.sortBy = 'name';
+      component.eventId = 3;
+
+      component.ngOnChanges();
+
+      expect(component.visibleSessions.length).toBe(2);
+    });
+
+    it('should sort the sessions correctly', () => {
+      component.sessions = <ISession[]>[
+        { name: 'session 1', level: 'intermediate' },
+        { name: 'session 3', level: 'intermediate' },
+        { name: 'session 2', level: 'beginner' }
+      ];
+
+      component.filterBy = 'all';
+      component.sortBy = 'name';
+      component.eventId = 3;
+
+      component.ngOnChanges();
+
+      expect(component.visibleSessions[2].name).toBe(
+        'session 3'
+      );
+    });
+  });
+});
