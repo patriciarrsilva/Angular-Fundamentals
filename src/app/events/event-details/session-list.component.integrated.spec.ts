af2b60604d1472dd4de8aa2141e60591fa4c65a2
Patrícia Silva <prrs89@gmail.com>
2021-01-06 14:14:18 +0000

    Testing Components with Deep Integrated Tests

--- src/app/events/event-details/session-list.component.integrated.spec.ts
+++ src/app/events/event-details/session-list.component.integrated.spec.ts
@@ -5,9 +5,12 @@ import {
   TestBed
 } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { CollapsibleWellComponent } from 'src/app/common';
 import { AuthService } from 'src/app/user/auth.service';
+import { DurationPipe } from '../shared';
 import { ISession } from '../shared/event.model';
 import { SessionListComponent } from './session-list.component';
+import { UpvoteComponent } from './upvote.component';
 import { VoterService } from './voter.service';
 
 describe('SessionListComponent', () => {
@@ -22,7 +25,12 @@ describe('SessionListComponent', () => {
 
     TestBed.configureTestingModule({
       imports: [],
-      declarations: [SessionListComponent],
+      declarations: [
+        SessionListComponent,
+        UpvoteComponent,
+        DurationPipe,
+        CollapsibleWellComponent
+      ],
       providers: [
         {
           provide: AuthService,
@@ -44,5 +52,31 @@ describe('SessionListComponent', () => {
     component = fixture.componentInstance;
     debugEl = fixture.debugElement;
     element = fixture.nativeElement;
+  });
+
+  describe('initial display', () => {
+    it('should have the correct session title', () => {
+      component.sessions = [
+        {
+          id: 3,
+          name: 'Session 1',
+          presenter: 'Joe',
+          duration: 1,
+          level: 'beginner',
+          abstract: 'abstract',
+          voters: ['john', 'bob']
+        }
+      ];
+      component.filterBy = 'all';
+      component.sortBy = 'name';
+      component.eventId = 4;
+
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expect(
+        element.querySelector('[well-title]').textContent
+      ).toContain('Session 1');
+    });
   });
 });
